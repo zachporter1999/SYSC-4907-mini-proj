100f00e5af5663d2f01aae9e704bdbe2339734ce
Zach Porter <zachporter@cmail.carleton.ca>
2021-11-20 15:10:49 -0500

    Fixed compile errors with project

--- IR_Sender/src/main.c
+++ IR_Sender/src/main.c
@@ -1,33 +1,41 @@
-#include <MKL25Z4.h>
-
-#include "timers.h"
+#include "LEDs.h"
+#include "IR.h"
 
 #define IR_SAMPLE_PERIOD 40000
 
 int main(void)
 {
-	// Setup
-	static unsigned avg_diff;
+	static unsigned on_brightness = 0, off_brightness = 0;
+	static int avg_diff;
+	static int diff;
+	unsigned n;
 
-	// unsigned n;
-
+	Init_ADC();
 	Init_RGB_LEDs();
 	Init_IR_LED();
-	Init_PIT(IR_SAMPLE_PERIOD);
-	Init_ADC();
 	Control_RGB_LEDs(0, 0, 0);
-	j Start_PIT();
 
-	// Main Loop
 	while (1)
 	{
-		// average the differences of IR Readings
-		if (n_IR_samples == 2 * NUM_SAMPLES_TO_AVG)
+		diff = 0;
+		for (n = 0; n < NUM_SAMPLES_TO_AVG; n++)
 		{
-			avg_diff = IR_diff / NUM_SAMPLES_TO_AVG;
-			n_IR_samples = 0;
+			// measure IR level with IRLED off
+			Control_IR_LED(0);
+			Delay_us(T_DELAY_OFF);
+			off_brightness = Measure_IR();
 
-			Display_Range((int)avg_diff);
+			// measure IR level with IRLED on
+			Control_IR_LED(1);
+			Delay_us(T_DELAY_ON);
+			on_brightness = Measure_IR();
+
+			// calculate difference
+			diff += on_brightness - off_brightness;
 		}
+		avg_diff = diff / NUM_SAMPLES_TO_AVG;
+
+		// light RGB LED according to range
+		Display_Range(avg_diff);
 	}
 }
