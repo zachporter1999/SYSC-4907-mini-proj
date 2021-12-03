e14250c743414bc7abb97e530972d1c874b469ee
Super-Sultan <erdemyanikomer@gmail.com>
2021-12-02 22:33:45 -0500

    pushing for posterity

--- LCD_Receiver/inc/switches.h
+++ LCD_Receiver/inc/switches.h
@@ -1,18 +1,17 @@
 #ifndef SWITCHES_H
 #define SWITCHES_H
 
-// All switches are on port E
-#define SW_UP_POS (21)
-#define SW_DN_POS (29)
-#define SW_LT_POS (30)
-#define SW_RT_POS (23)
-#define SW_CR_POS (22)
+// All switches are on port D
+#define SW_0_POS (0)
+#define SW_1_POS (1)
+#define SW_2_POS (2)
+#define SW_3_POS (3)
 
 // Macro to read switches returns state switches, active low
 #define READ_SWITCHES  (PTE->PDIR)  
 
 // Function prototypes
-extern void Init_5way_Switch(void);
+extern void Init_4way_Switch(void);
 
 #endif
 // *******************************ARM University Program Copyright © ARM Ltd 2013*************************************   
