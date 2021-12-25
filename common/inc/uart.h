d88ce50d638b80bfe1501bf717abdf0ccc775bba
Zach Porter <zachporter@cmail.carleton.ca>
2021-12-25 16:50:37 -0500

    Cleanup of uart code

--- common/inc/uart.h
+++ common/inc/uart.h
@@ -21,9 +21,6 @@ volatile extern uart_transceiver_t uart2_transceiver;
 #define UART_OVERSAMPLE (16)
 #define BUS_CLOCK 			(24e6)
 
-//uart_transeiver_t Init_UART1(uint32_t baud_rate, size_t data_bytes);
-//uart_transeiver_t Init_UART2(uint32_t baud_rate, size_t data_bytes);
-
 void Init_UART1(uint32_t baud_rate, size_t data_bytes, uart_transceiver_t *transceiver);
 void Init_UART2(uint32_t baud_rate, size_t data_bytes, uart_transceiver_t *transceiver);
 
@@ -36,8 +33,8 @@ uint8_t UART2_Receive_Poll(void);
 
 // interrupts
 void send_data(uart_transceiver_t *transeiver, void* data);
-uint32_t Get_Num_Rx_Chars_Available(void);
-uint32_t get_data(uart_transceiver_t* transeiver);
+uint32_t Get_Num_Rx_Chars_Available(uart_transceiver_t *transceiver);
+void get_data(uart_transceiver_t* transeiver, void* data);
 
 extern Q_T TxQ, RxQ;
 extern uint8_t CR_received;
--- common/src/uart.c
+++ common/src/uart.c
@@ -5,91 +5,6 @@
 Q_T TxQ, RxQ;
 uint8_t CR_received = 0;
 
-/*
-uart_transceiver_t Init_UART1(uint32_t baud_rate, size_t data_bytes) {
-	uart_transceiver_t transceiver;
-	uint32_t divisor;
-	
-	// enable clock to UART and Port E
-	SIM->SCGC4 |= SIM_SCGC4_UART1_MASK;
-	SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK;
-
-
-	// select UART pins
-	PORTE->PCR[0] = PORT_PCR_MUX(3);
-	PORTE->PCR[1] = PORT_PCR_MUX(3);
-	
-	UART1->C2 &=  ~(UARTLP_C2_TE_MASK | UARTLP_C2_RE_MASK);
-		
-	// Set baud rate to baud rate
-	divisor = (uint32_t)BUS_CLOCK/(baud_rate*16);
-	UART1->BDH = UART_BDH_SBR(divisor>>8);
-	UART1->BDL = UART_BDL_SBR(divisor);
-	
-	// No parity, 9 bits, two stop bits, other settings;
-	UART1->C1 = UART_C1_M_MASK | UART_C1_PE_MASK | UART_C1_PT_MASK; 
-	UART1->S2 = 0;
-	UART1->C3 = 0;
-	
-// Enable transmitter and receiver but not interrupts
-	UART1->C2 = UART_C2_TE_MASK | UART_C2_RE_MASK;
-
-	transceiver.bytes_per_data = data_bytes;
-	
-	NVIC_SetPriority(UART1_IRQn, 128); // 0, 64, 128 or 192
-	NVIC_ClearPendingIRQ(UART1_IRQn); 
-	NVIC_EnableIRQ(UART1_IRQn);
-
-	UART1->C2 |= UART_C2_TIE_MASK | UART_C2_RIE_MASK;
-
-	Q_Init(&transceiver.TxQ);
-	Q_Init(&transceiver.RxQ);
-	
-	return transceiver;
-}
-
-uart_transceiver_t Init_UART2(uint32_t baud_rate, size_t data_bytes) {
-	uart_transceiver_t transceiver;
-	uint32_t divisor;
-	
-	// enable clock to UART and Port E
-	SIM->SCGC4 |= SIM_SCGC4_UART2_MASK;
-	SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK;
-
-
-	// select UART pins
-	PORTE->PCR[22] = PORT_PCR_MUX(3);
-	PORTE->PCR[23] = PORT_PCR_MUX(3);
-	
-	UART2->C2 &=  ~(UARTLP_C2_TE_MASK | UARTLP_C2_RE_MASK);
-		
-	// Set baud rate to baud rate
-	divisor = (uint32_t)BUS_CLOCK/(baud_rate*16);
-	UART2->BDH = UART_BDH_SBR(divisor>>8);
-	UART2->BDL = UART_BDL_SBR(divisor);
-	
-	// No parity, 9 bits, two stop bits, other settings;
-	UART2->C1 = UART_C1_M_MASK | UART_C1_PE_MASK | UART_C1_PT_MASK; 
-	UART2->S2 = 0;
-	UART2->C3 = 0;
-	
-	UART2->C2 = UART_C2_TE_MASK | UART_C2_RE_MASK;
-
-	transceiver.bytes_per_data = data_bytes;
-	
-	NVIC_SetPriority(UART2_IRQn, 128); // 0, 64, 128 or 192
-	NVIC_ClearPendingIRQ(UART2_IRQn); 
-	NVIC_EnableIRQ(UART2_IRQn);
-
-	UART2->C2 |= UART_C2_TIE_MASK | UART_C2_RIE_MASK;
-
-	Q_Init(&transceiver.TxQ);
-	Q_Init(&transceiver.RxQ);
-
-	return transceiver;
-}
-*/
-
 void Init_UART1(uint32_t baud_rate, size_t data_bytes, uart_transceiver_t *transceiver) {
 	uint32_t divisor;
 	
@@ -216,21 +131,16 @@ void UART1_IRQHandler(void) {
 			if (c == '\r') {
 				CR_received++;
 			}
-		} else {
-			// error - queue full.
-			while (1)
-				;
 		}
 	}
+	/*
 	if (UART1->S1 & (UART_S1_OR_MASK |UART_S1_NF_MASK | 
 		UART_S1_FE_MASK | UART_S1_PF_MASK)) {
-			// handle the error
-			// clear the flag
-			/*
+
 			UART1->S1 = UART_S1_OR_MASK | UART_S1_NF_MASK | 
 				UART_S1_FE_MASK | UART_S1_PF_MASK;
-		 */
 		}
+	*/
 }
 
 void UART2_IRQHandler(void) {
@@ -253,21 +163,15 @@ void UART2_IRQHandler(void) {
 			if (c == '\r') {
 				CR_received++;
 			}
-		} else {
-			// error - queue full.
-			while (1)
-				;
 		}
 	}
+	/*
 	if (UART2->S1 & (UART_S1_OR_MASK |UART_S1_NF_MASK | 
 		UART_S1_FE_MASK | UART_S1_PF_MASK)) {
-			// handle the error
-			// clear the flag
-			/*
 			UART2->S1 = UART_S1_OR_MASK | UART_S1_NF_MASK | 
 				UART_S1_FE_MASK | UART_S1_PF_MASK;
-		 */
 		}
+	*/
 }
 
 /*
@@ -286,9 +190,10 @@ void send_data(uart_transceiver_t *transceiver, void* data)
 	for (int byte_idx = (int)transceiver->bytes_per_data - 1; byte_idx >= 0; byte_idx--)
 	{
 		// Wait while Queue is full.
-		while(Q_Full(&transceiver->TxQ));
-
-		Q_Enqueue(&transceiver->TxQ, (uint8_t)(transmit_data >> 8*byte_idx));
+		if (!Q_Full(&transceiver->TxQ))
+		{
+			Q_Enqueue(&transceiver->TxQ, (uint8_t)(transmit_data >> 8*byte_idx));
+		}
 	}
 
 	// Start transmitter if it isn't already running
@@ -297,23 +202,24 @@ void send_data(uart_transceiver_t *transceiver, void* data)
 	}
 }
 
-uint32_t Get_Num_Rx_Chars_Available(void) {
-	return Q_Size(&RxQ);
+uint32_t Get_Num_Rx_Chars_Available(uart_transceiver_t *transceiver) {
+	return Q_Size(&transceiver->RxQ);
 }
 
-uint32_t get_data(uart_transceiver_t* transceiver)
+void get_data(uart_transceiver_t* transceiver, void* data)
 {
 	uint32_t received_data = 0;
 
-	for (int byte_idx = (int)transceiver->bytes_per_data - 1; byte_idx >= 0; byte_idx++)
+	for (int byte_idx = (int)transceiver->bytes_per_data - 1; byte_idx >= 0; byte_idx--)
 	{
 		// Wait while Queue is empty.
-		while (Q_Empty(&transceiver->RxQ));
-
-		// Retreive databytes from the queue
-		uint8_t byte = Q_Dequeue(&transceiver->RxQ);
-		received_data |= (uint32_t)(byte << 8 * byte_idx);
+		if (!Q_Empty(&transceiver->RxQ))
+		{
+			// Retreive databytes from the queue
+			uint8_t byte = Q_Dequeue(&transceiver->RxQ);
+			received_data |= (uint32_t)(byte << 8 * byte_idx);
+		}
 	}
 
-	return received_data;
+	*(uint32_t*)data = received_data;
 }
