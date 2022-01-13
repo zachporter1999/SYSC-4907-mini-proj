3f7e8d4e096fc4007acb490474cc26a15749e71a
Zach Porter <zach.ld.porter@gmail.com>
2022-01-13 15:36:08 -0500

    Added basic test structure to tests

--- tests/agent-test/src/main.c
+++ tests/agent-test/src/main.c
@@ -2,7 +2,26 @@
 
 #include "drivers/uart.h"
 
-int main (void)
+// Controls for selecting which tests to run
+#define TEST_UART 1
+#define TEST_I2C  0
+#define TEST_SPI  0
+
+int main(void)
 {
-	
+#if TEST_UART
+
+	Init_UART1(9600, sizeof(uint8_t), NULL);
+
+#endif
+
+#if TEST_I2C
+
+
+#endif
+
+#if TEST_SPI
+
+
+#endif
 }
