8f62d315752e346426049953c058ce8d9dcae30b
Zach Porter <zachporter@cmail.carleton.ca>
2022-01-13 14:47:30 -0500

    Updated libserial structure. resturctured source and include dirs

--- libserial/src/drivers/uart.c
+++ libserial/src/drivers/uart.c
@@ -1,4 +1,4 @@
-#include "uart.h"
+#include "drivers/uart.h"
 #include <stdint.h>
 
 
@@ -7,91 +7,6 @@ uint8_t CR_received = 0;
 
 volatile uart_transceiver_t uart1_transceiver;
 volatile uart_transceiver_t uart2_transceiver;
-
-/*
-uart_transceiver_t Init_UART1(uint32_t baud_rate, size_t data_bytes) {
-	uart_transceiver_t transceiver;
-	uint32_t divisor;
-	
-	// enable clock to UART and Port E
-	SIM->SCGC4 |= SIM_SCGC4_UART1_MASK;
-	SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK;
-
-
-	// select UART pins
-	PORTE->PCR[0] = PORT_PCR_MUX(3);
-	PORTE->PCR[1] = PORT_PCR_MUX(3);
-	
-	UART1->C2 &=  ~(UARTLP_C2_TE_MASK | UARTLP_C2_RE_MASK);
-		
-	// Set baud rate to baud rate
-	divisor = (uint32_t)BUS_CLOCK/(baud_rate*16);
-	UART1->BDH = UART_BDH_SBR(divisor>>8);
-	UART1->BDL = UART_BDL_SBR(divisor);
-	
-	// No parity, 9 bits, two stop bits, other settings;
-	UART1->C1 = UART_C1_M_MASK | UART_C1_PE_MASK | UART_C1_PT_MASK; 
-	UART1->S2 = 0;
-	UART1->C3 = 0;
-	
-// Enable transmitter and receiver but not interrupts
-	UART1->C2 = UART_C2_TE_MASK | UART_C2_RE_MASK;
-
-	transceiver.bytes_per_data = data_bytes;
-	
-	NVIC_SetPriority(UART1_IRQn, 128); // 0, 64, 128 or 192
-	NVIC_ClearPendingIRQ(UART1_IRQn); 
-	NVIC_EnableIRQ(UART1_IRQn);
-
-	UART1->C2 |= UART_C2_TIE_MASK | UART_C2_RIE_MASK;
-
-	Q_Init(&transceiver.TxQ);
-	Q_Init(&transceiver.RxQ);
-	
-	return transceiver;
-}
-
-uart_transceiver_t Init_UART2(uint32_t baud_rate, size_t data_bytes) {
-	uart_transceiver_t transceiver;
-	uint32_t divisor;
-	
-	// enable clock to UART and Port E
-	SIM->SCGC4 |= SIM_SCGC4_UART2_MASK;
-	SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK;
-
-
-	// select UART pins
-	PORTE->PCR[22] = PORT_PCR_MUX(3);
-	PORTE->PCR[23] = PORT_PCR_MUX(3);
-	
-	UART2->C2 &=  ~(UARTLP_C2_TE_MASK | UARTLP_C2_RE_MASK);
-		
-	// Set baud rate to baud rate
-	divisor = (uint32_t)BUS_CLOCK/(baud_rate*16);
-	UART2->BDH = UART_BDH_SBR(divisor>>8);
-	UART2->BDL = UART_BDL_SBR(divisor);
-	
-	// No parity, 9 bits, two stop bits, other settings;
-	UART2->C1 = UART_C1_M_MASK | UART_C1_PE_MASK | UART_C1_PT_MASK; 
-	UART2->S2 = 0;
-	UART2->C3 = 0;
-	
-	UART2->C2 = UART_C2_TE_MASK | UART_C2_RE_MASK;
-
-	transceiver.bytes_per_data = data_bytes;
-	
-	NVIC_SetPriority(UART2_IRQn, 128); // 0, 64, 128 or 192
-	NVIC_ClearPendingIRQ(UART2_IRQn); 
-	NVIC_EnableIRQ(UART2_IRQn);
-
-	UART2->C2 |= UART_C2_TIE_MASK | UART_C2_RIE_MASK;
-
-	Q_Init(&transceiver.TxQ);
-	Q_Init(&transceiver.RxQ);
-
-	return transceiver;
-}
-*/
 
 void Init_UART1(uint32_t baud_rate, size_t data_bytes, uart_transceiver_t *transceiver) {
 	uint32_t divisor;
@@ -174,30 +89,6 @@ void Init_UART2(uint32_t baud_rate, size_t data_bytes, uart_transceiver_t *trans
 	Q_Init(&transceiver->RxQ);
 
 }
-
-void UART1_Transmit_Poll(uint8_t data) {
-		while (!(UART1->S1 & UART_S1_TDRE_MASK))
-			;
-		UART1->D = data;
-}	
-
-uint8_t UART1_Receive_Poll(void) {
-		while (!(UART1->S1 & UART_S1_RDRF_MASK))
-			;
-		return UART1->D;
-}	
-
-void UART2_Transmit_Poll(uint8_t data) {
-		while (!(UART1->S1 & UART_S1_TDRE_MASK))
-			;
-		UART1->D = data;
-}	
-
-uint8_t UART2_Receive_Poll(void) {
-		while (!(UART1->S1 & UART_S1_RDRF_MASK))
-			;
-		return UART1->D;
-}	
 
 void UART1_IRQHandler(void) {
 	uint8_t c;
